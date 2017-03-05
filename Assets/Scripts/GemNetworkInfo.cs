57101592211e1af7b0c00ed3245d72493c014d00
shikang <shikang.n@digipen.edu>
2017-03-05 22:41:34 +0800

    Optimise networking

---
+++ Assets/Scripts/GemNetworkInfo.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GemNetworkInfo : MonoBehaviour
+{
+	private int m_ID;
+	private Vector3 m_HalfDimension;
+	private Gem m_Gem = null;
+	private bool m_bOtherLinked = false;
+
+	public int ID
+	{
+		get
+		{
+			return m_ID;
+		}
+		set
+		{
+			m_ID = value;
+		}
+	}
+
+	public bool OtherLinked
+	{
+		get
+		{
+			return m_bOtherLinked;
+		}
+	}
+
+	// Use this for initialization
+	void Start ()
+	{
+		m_HalfDimension = Camera.main.ScreenToWorldPoint( new Vector3( ( Screen.width ), ( Screen.height ) ) );
+		m_Gem = GetComponent<Gem>();
+
+		m_bOtherLinked = false;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+
+	public void LinkNetworkGem( bool linked )
+	{
+		if ( m_bOtherLinked != linked )
+		{
+			GemSpawner spawner = GameObject.Find( "GemSpawner" ).GetComponent<GemSpawner>();
+
+			// If link, scale up change sprite
+			if ( linked )
+			{
+				spawner.SetLinkGemEffect( m_Gem );
+			}
+			// If not linked, scale back change sprite
+			else
+			{
+				spawner.UnsetLinkGemEffect( m_Gem );
+			}
+		}
+		m_bOtherLinked = linked;
+	}
+}
---
+++ Assets/Scripts/NetworkGameTime.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkGameTime : Photon.MonoBehaviour
+{
+	const float TIME_DIFF_THRESHOLD = 2.0f;	//!< In seconds
+
+	private float m_fGameTime = 0.0f;
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		m_fGameTime += Time.deltaTime;
+	}
+
+	public float GetGameTime()
+	{
+		return m_fGameTime;
+	}
+
+	void OnPhotonSerializeView( PhotonStream stream, PhotonMessageInfo info )
+	{
+		if ( stream.isWriting )
+		{
+			// Sending data over
+			stream.SendNext( m_fGameTime );
+		}
+		else
+		{
+			// Receiving data
+			float gameTime = ( float )stream.ReceiveNext();
+
+			// Time diff too huge
+			if ( Mathf.Abs( gameTime - m_fGameTime ) > TIME_DIFF_THRESHOLD )
+			{
+				m_fGameTime = gameTime;
+			}
+		}
+	}
+
+	void OnPhotonInstantiate( PhotonMessageInfo info )
+	{
+		if ( !NetworkManager.IsPlayerOne() )
+		{
+			GemSpawner spawner = GameObject.Find( "GemSpawner" ).GetComponent<GemSpawner>();
+			spawner.AddNetworkGameTimer( this );
+		}
+	}
+}
