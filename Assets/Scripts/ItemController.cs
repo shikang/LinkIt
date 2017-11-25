53193da33978765bd45aacf88a73950712540d30
SolidRocker <kamtk.tyler@gmail.com>
2017-11-25 16:51:25 +0800

    Added Audio Manager and Vibration Added item usage to main menu

---
+++ Assets/Scripts/ItemController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum ITEMLIST
+{
+	SLOW_GEMS = 0,
+	MULTIPLY_SCORE,
+	DESTROY_COLOR,
+	LINK_GEMS
+};
+
+public class ItemController : MonoBehaviour
+{
+	public GameObject m_ItemSlot1;
+	public GameObject m_ItemSlot2;
+	public GameObject m_ItemSlot3;
+	public GameObject m_ItemSlot4;
+
+	bool [] m_bItemsActivated;
+
+	void Start()
+	{
+		m_bItemsActivated = new bool[4];
+		SetItems();
+	}
+
+	void Update()
+	{
+	}
+
+	void SetItems ()
+	{
+		m_ItemSlot1.GetComponent<ItemMainMenu>().SetData("Faster Charge",	350, "Powerup gauge fills up 2x faster");
+		m_ItemSlot2.GetComponent<ItemMainMenu>().SetData("Score 2x",		500, "Multiply your score by 2x");
+		m_ItemSlot3.GetComponent<ItemMainMenu>().SetData("HP Recovery",		650, "HP recovers 2x faster");
+		m_ItemSlot4.GetComponent<ItemMainMenu>().SetData("Longer Powerups",	1000, "Powerups last 2x longer");
+	}
+}
---
+++ Assets/Scripts/ItemMainMenu.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ItemMainMenu : MonoBehaviour
+{
+	bool m_bIsActivated;
+	string m_sName;
+	string m_sDesc;
+	uint m_uCost;
+
+	public GameObject m_ItemName;
+	public GameObject m_Cost;
+	public GameObject m_Desc;
+
+	Color m_cActive = new Color(0.0f, 0.5f, 1.0f, 1.0f);
+	Color m_cInactive = Color.grey;
+
+	void Start()
+	{
+	}
+
+	void Update()
+	{
+	}
+
+	public void SetData(string name_, uint cost_, string desc_)
+	{
+		SetName(name_);
+		SetCost(cost_);
+		SetDesc(desc_);
+		m_bIsActivated = false;
+		GetComponent<Image>().color = m_cInactive;
+	}
+
+	public void SetName (string name_)
+	{
+		m_sName = name_;
+		m_ItemName.GetComponent<Text>().text = m_sName;
+	}
+
+	public void SetCost (uint cost_)
+	{
+		m_uCost = cost_;
+		m_Cost.GetComponent<Text>().text = m_uCost.ToString();
+	}
+
+	public void SetDesc (string desc_)
+	{
+		m_sDesc = desc_;
+		m_Desc.GetComponent<Text>().text = m_sDesc;
+	}
+
+	public void SetActive()
+	{
+		m_bIsActivated = !m_bIsActivated;
+
+		if(m_bIsActivated)
+			GetComponent<Image>().color = m_cActive;
+		else
+			GetComponent<Image>().color = m_cInactive;
+	}
+
+	public string GetName()
+	{
+		return m_sName;
+	}
+
+	public uint GetCost()
+	{
+		return m_uCost;
+	}
+
+	public string GetDesc()
+	{
+		return m_sDesc;
+	}
+
+	public bool IsActive()
+	{
+		return m_bIsActivated;
+	}
+}
