932a5a40383d6ef93354fbf2ae15ca10fdc019f8
Shi Kang <shikang.neoh@razer.com>
2019-04-22 20:03:11 +0800

    Remove vibrator

--- Assets/Scripts/VibManager.cs
+++ Assets/Scripts/VibManager.cs
@@ -11,11 +11,14 @@ public enum VIBSTR
 public class VibManager : MonoBehaviour
 {
 #if UNITY_ANDROID && !UNITY_EDITOR
-	public static AndroidJavaClass unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
-	public static AndroidJavaObject currentActivity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
-	public static AndroidJavaObject vibrator = currentActivity.Call<AndroidJavaObject>("getSystemService", "vibrator");
+	//public static AndroidJavaClass unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+	//public static AndroidJavaObject currentActivity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
+	//public static AndroidJavaObject vibrator = currentActivity.Call<AndroidJavaObject>("getSystemService", "vibrator");
+    public static AndroidJavaClass unityPlayer;
+	public static AndroidJavaObject currentActivity;
+	public static AndroidJavaObject vibrator;
 #else
-	public static AndroidJavaClass unityPlayer;
+    public static AndroidJavaClass unityPlayer;
 	public static AndroidJavaObject currentActivity;
 	public static AndroidJavaObject vibrator;
 #endif
